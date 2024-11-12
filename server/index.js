d1ab3cfed707155b0720342d1fcad62dc859dbb2
azedine ouhadou <aouhadou@student.1337.ma>
2024-11-12 20:26:22 +0100

    create todo

--- server/index.js
+++ server/index.js
@@ -11,6 +11,21 @@ app.use(express.json());
 
 // create a todo
 
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
 // get all todos
 
 // get a todo
